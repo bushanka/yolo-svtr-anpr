a690afe11becf44b2b9d74cbdb19836ae5c7f4bc
bushanka <bushanka2805@gmail.com>
2023-12-14 19:33:46 +0300

    from server changes

--- app/routes/recognize.py
+++ app/routes/recognize.py
@@ -7,7 +7,8 @@ import numpy as np
 import os
 import cv2
 from app import anpr
-from app.models.recognize import RecognitionOutput
+from app.models.recognize import RecognitionOutput, Plate
+
 
 
 router = APIRouter(
@@ -17,6 +18,8 @@ router = APIRouter(
 RUNTYPE = os.getenv('RUNTYPE') if os.getenv('RUNTYPE') else 'cpu'
 model = anpr.Anpr(RUNTYPE)
 
+
+
 @router.post(
     '/recognize',
     responses={
@@ -25,16 +28,18 @@ model = anpr.Anpr(RUNTYPE)
         },
     }
 )
-def recognize(image_bytes: bytes = File()) -> RecognitionOutput:
-    nparr = np.fromstring(image_bytes, np.uint8)
-    print(nparr)
-    print(len(nparr))
+def recognize(images: bytes = File()) -> RecognitionOutput:
+    nparr = np.fromstring(images, np.uint8)
     try:
         img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     except cv2.error as ex_cv2:
         print(f"Error in decoding image: {ex_cv2}")
         return RecognitionOutput(
-            plate="no detection, error in decoding image (maybe empty image sended)",
+            results=[
+                Plate(
+                    plate="no detection, error in decoding image (maybe empty image sended)"
+                )
+            ]
             confidence=-1,
             predict_time=0.0
         )
@@ -44,24 +49,36 @@ def recognize(image_bytes: bytes = File()) -> RecognitionOutput:
     except AttributeError as ex_attrerror:
         print(ex_attrerror)
         return RecognitionOutput(
-            plate=f"no detection, error in model inference (maybe image shape is 0) recieved image len: {len(nparr)}",
+            results=[
+                Plate(
+                    plate=f"no detection, error in model inference (maybe image shape is 0) recieved image len: {len(nparr)}"
+                )
+            ]
             confidence=-1,
             predict_time=0.0
         )
 
     if not result['recognition']:
         return RecognitionOutput(
-            plate='no detection',
+            results=[
+                Plate(
+                    plate=f"no detection"
+                )
+            ]
             confidence=-1,
             predict_time=result['detection']['speed']['total']
         )
-
     plate_num = result['recognition']['text']
     total_prob = result['detection']['prob'] * result['recognition']['prob']
     total_time = float(result['detection']['speed']['total']) + float(result['recognition']['speed']['total'])
 
+
     return RecognitionOutput(
-        plate=plate_num,
+        results=[
+            Plate(
+                plate=plate_num
+            )
+        ]
         confidence=total_prob,
         predict_time=total_time
     )
