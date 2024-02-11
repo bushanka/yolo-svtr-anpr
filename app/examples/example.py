84a19a64ac63232d5cfce59bda82455fef134cf9
bushanka <bushanka2805@gmail.com>
2024-02-11 14:48:50 +0300

    pep8 formatted, docker updated, requirements added

--- app/examples/example.py
+++ app/examples/example.py
@@ -1,12 +1,10 @@
 import anpr
 
-
 if __name__ == '__main__':
     import pprint
     import cv2
 
-
-    it = anpr.Anpr('cpu') # also 'cpu' and 'trt' is available (trt not tested)
+    it = anpr.Anpr('cpu')  # also 'cpu' and 'trt' is available (trt not tested)
 
     # warmup
     image = cv2.imread('/home/bush/project/anpr/photo_2023-10-16_13-37-16.jpg')
